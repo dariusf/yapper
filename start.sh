da74f41b330a9fcdeaa59c90a4b4e8acab7132da
Darius Foo <darius.foo.tw@gmail.com>
2026-02-08 19:28:59 +0800

    More convenient way to start

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# get absolute path to directory of this script
 set -x
-until $@; do
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+until node "$SCRIPT_DIR/yapper.js"; do
   echo "crashed with exit code $?" >&2
   sleep 1
 done
